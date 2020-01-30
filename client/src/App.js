d9b879b2314a548b6eb65c8f351e2c38af464370
Chris Matian <chris.matian@race.com>
2020-01-29 16:31:17 -0800

    Refactor app.js to only consist of routes, provider, or nav elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import LandingPage from "./components/layouts/LandingPage";
+import Home from "./components/layouts/Home";
 import Maps from "./components/maps/Maps";
 import Search from "./components/maps/Search";
 import Biz from "./components/business/Biz";
@@ -12,62 +12,22 @@ import Saved from "./components/maps/Saved";
 // App specific styling
 import "./App.scss";
 
+// Only routes, the provider context API, or nav elements should be on App.js
 class App extends React.Component {
-    state = {
-        apiResponse: "",
-        lat: null,
-        long: null,
-    };
-
-    callAPI() {
-        fetch("http://localhost:5000/testapi")
-            .then(res => res.text())
-            .then(res =>
-                this.setState({
-                    apiResponse: res,
-                })
-            )
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
-
-        // get user loc
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // set state
-                this.setState({ lat: position.coords.latitude });
-                this.setState({ long: position.coords.longitude });
-            },
-            err => console.log(err)
-        );
-    }
-
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <p>
-                        API Response: <br />
-                        {this.state.apiResponse} <br />
-                        latitude: {this.state.lat} <br />
-                        longitude: {this.state.long}
-                    </p>
-
-                    <div>
-                        <BrowserRouter>
-                            <Route path="/" exact component={LandingPage}></Route>
-                            <Route path="/maps" exact component={Maps}></Route>
-                            <Route path="/maps/search" exact component={Search}></Route>
-                            <Route path="/biz" exact component={Biz}></Route>
-                            <Route path="/biz/rate" exact component={Rates}></Route>
-                            <Route path="/biz/reserve" exact component={Reserve}></Route>
-                            <Route path="/users/reservations" exact component={Reservations}></Route>
-                            <Route path="/maps/users/saved" exact component={Saved}></Route>
-                        </BrowserRouter>
-                    </div>
-                </header>
+                <BrowserRouter>
+                    {/* Can optionally wrap all routes in a switch if we only want one matched route to render */}
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/maps" exact component={Maps}></Route>
+                    <Route path="/maps/search" exact component={Search}></Route>
+                    <Route path="/biz" exact component={Biz}></Route>
+                    <Route path="/biz/rate" exact component={Rates}></Route>
+                    <Route path="/biz/reserve" exact component={Reserve}></Route>
+                    <Route path="/users/reservations" exact component={Reservations}></Route>
+                    <Route path="/maps/users/saved" exact component={Saved}></Route>
+                </BrowserRouter>
             </div>
         );
     }
