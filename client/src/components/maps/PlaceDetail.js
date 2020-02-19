5fef2bc7d9084ddc6ae7addb19eb4c3e23c2cfc5
Melisa Lee <lee.melisa@gmail.com>
2020-02-18 18:26:10 -0800

    Added currently selected place and details to place card

--- client/src/components/maps/PlaceDetail.js
+++ client/src/components/maps/PlaceDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import './PlaceSelected.scss'
 
+// Display extended details for matches display of hours, location, and distance
 class PlaceDetail extends React.Component {
     render() {
         return(
